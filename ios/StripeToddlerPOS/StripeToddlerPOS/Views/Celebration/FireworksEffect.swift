0bfef081de29d5314fea9f727f82856c0a25cbfa
Nick Brett <nick.brett1@gmail.com>
2026-08-06 02:20:43 +0000

    fix(ios): refactor FireworksEffect engine to eliminate onChange Date state mutation warning

--- ios/StripeToddlerPOS/StripeToddlerPOS/Views/Celebration/FireworksEffect.swift
+++ ios/StripeToddlerPOS/StripeToddlerPOS/Views/Celebration/FireworksEffect.swift
@@ -12,45 +12,68 @@ struct CelebrationParticle: Identifiable {
     var alpha: Double = 1.0
 }
 
-// MARK: - Continuous Fireworks Particle Effect (TimelineView + Canvas)
-struct FireworksEffect: View {
-    @State private var particles: [CelebrationParticle] = []
-    @State private var frameCounter = 0
-
-    var body: some View {
-        TimelineView(.animation) { timeline in
-            Canvas { context, size in
-                for particle in particles {
-                    var pContext = context
-                    pContext.opacity = particle.alpha
-                    let rect = CGRect(
-                        x: particle.x - particle.size / 2,
-                        y: particle.y - particle.size / 2,
-                        width: particle.size,
-                        height: particle.size
-                    )
-                    pContext.fill(Path(ellipseIn: rect), with: .color(particle.color))
-                }
-            }
-            .onAppear {
-                spawnInitialExplosions()
-            }
-            .onChange(of: timeline.date) { _ in
-                updateParticles()
+// MARK: - Fireworks Engine (Zero State Mutations in View Body)
+final class FireworksSystem {
+    private var particles: [CelebrationParticle] = []
+    private var lastUpdateDate: Date? = nil
+    private var lastBurstDate: Date = Date()
+    
+    func updateAndDraw(context: inout GraphicsContext, size: CGSize, date: Date) {
+        let delta: Double
+        if let lastDate = lastUpdateDate {
+            delta = min(date.timeIntervalSince(lastDate), 0.05)
+        } else {
+            delta = 1.0 / 60.0
+            spawnInitialExplosions(size: size)
+        }
+        lastUpdateDate = date
+        
+        // Continuously launch firework bursts every 0.4 seconds for toddlers
+        if date.timeIntervalSince(lastBurstDate) > 0.4 {
+            lastBurstDate = date
+            let w = size.width > 0 ? size.width : UIScreen.main.bounds.width
+            let h = size.height > 0 ? size.height : UIScreen.main.bounds.height
+            let randomX = Double.random(in: (w * 0.15)...(w * 0.85))
+            let randomY = Double.random(in: (h * 0.15)...(h * 0.45))
+            spawnBurst(x: randomX, y: randomY)
+        }
+        
+        // Time-delta normalized physics update (60fps baseline)
+        let timeScale = delta * 60.0
+        var index = 0
+        while index < particles.count {
+            particles[index].x += particles[index].vx * timeScale
+            particles[index].y += particles[index].vy * timeScale
+            particles[index].vy += 0.08 * timeScale // Gravity
+            particles[index].alpha -= 0.006 * timeScale // Fade out
+            
+            if particles[index].alpha <= 0 {
+                particles.remove(at: index)
+            } else {
+                let p = particles[index]
+                var pContext = context
+                pContext.opacity = p.alpha
+                let rect = CGRect(
+                    x: p.x - p.size / 2,
+                    y: p.y - p.size / 2,
+                    width: p.size,
+                    height: p.size
+                )
+                pContext.fill(Path(ellipseIn: rect), with: .color(p.color))
+                index += 1
             }
         }
     }
-
-    private func spawnInitialExplosions() {
-        let screenWidth = UIScreen.main.bounds.width
-        let screenHeight = UIScreen.main.bounds.height
-
-        spawnBurst(x: screenWidth * 0.25, y: screenHeight * 0.3)
-        spawnBurst(x: screenWidth * 0.50, y: screenHeight * 0.25)
-        spawnBurst(x: screenWidth * 0.75, y: screenHeight * 0.3)
+    
+    private func spawnInitialExplosions(size: CGSize) {
+        let w = size.width > 0 ? size.width : UIScreen.main.bounds.width
+        let h = size.height > 0 ? size.height : UIScreen.main.bounds.height
+        spawnBurst(x: w * 0.25, y: h * 0.3)
+        spawnBurst(x: w * 0.50, y: h * 0.25)
+        spawnBurst(x: w * 0.75, y: h * 0.3)
     }
 
-    private func spawnBurst(x: CGFloat, y: CGFloat) {
+    private func spawnBurst(x: Double, y: Double) {
         let colors: [Color] = [.toddlerBlue, .toddlerGreen, .toddlerRed, .toddlerYellow, .purple, .orange, .pink]
         let particleCount = Int.random(in: 35...50)
 
@@ -59,8 +82,8 @@ struct FireworksEffect: View {
             let speed = Double.random(in: 2...9)
             particles.append(
                 CelebrationParticle(
-                    x: Double(x),
-                    y: Double(y),
+                    x: x,
+                    y: y,
                     vx: cos(angle) * speed,
                     vy: sin(angle) * speed,
                     color: colors.randomElement() ?? .toddlerYellow,
@@ -69,27 +92,17 @@ struct FireworksEffect: View {
             )
         }
     }
+}
 
-    private func updateParticles() {
-        frameCounter += 1
+// MARK: - Continuous Fireworks Particle Effect (TimelineView + Canvas)
+struct FireworksEffect: View {
+    private let system = FireworksSystem()
 
-        // Continuously launch new firework bursts every 25 frames (~0.4s) for endless toddler fun!
-        if frameCounter % 25 == 0 {
-            let screenWidth = UIScreen.main.bounds.width
-            let screenHeight = UIScreen.main.bounds.height
-            let randomX = CGFloat.random(in: (screenWidth * 0.15)...(screenWidth * 0.85))
-            let randomY = CGFloat.random(in: (screenHeight * 0.15)...(screenHeight * 0.45))
-            spawnBurst(x: randomX, y: randomY)
+    var body: some View {
+        TimelineView(.animation) { timeline in
+            Canvas { context, size in
+                system.updateAndDraw(context: &context, size: size, date: timeline.date)
+            }
         }
-
-        for i in 0..<particles.count {
-            particles[i].x += particles[i].vx
-            particles[i].y += particles[i].vy
-            particles[i].vy += 0.08 // Gentle gravity
-            particles[i].alpha -= 0.006 // Slow fade so fireworks linger much longer
-        }
-
-        // Prune faded particles
-        particles.removeAll { $0.alpha <= 0 }
     }
 }
